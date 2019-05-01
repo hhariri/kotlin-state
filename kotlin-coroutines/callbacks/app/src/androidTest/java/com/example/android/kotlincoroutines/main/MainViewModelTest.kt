ff582c05c0c753802c19eaba631d3dd3d48c2485
hadihariri <mail@hadihariri.com>
2019-05-01 22:08:34 +0200

    Coroutine samples

---
+++ kotlin-coroutines/callbacks/app/src/androidTest/java/com/example/android/kotlincoroutines/main/MainViewModelTest.kt
@@ -0,0 +1,112 @@
+package com.example.android.kotlincoroutines.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.kotlincoroutines.main.fakes.MainNetworkFake
+import com.example.android.kotlincoroutines.main.fakes.TitleDaoFake
+import com.example.android.kotlincoroutines.main.fakes.makeSuccessCall
+import com.example.android.kotlincoroutines.test.util.captureValues
+import com.example.android.kotlincoroutines.test.util.getValueForTest
+import com.example.android.kotlincoroutines.util.FakeNetworkCall
+import com.example.android.kotlincoroutines.util.FakeNetworkException
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.concurrent.TimeUnit.SECONDS
+
+@RunWith(JUnit4::class)
+class MainViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun loadsTitleByDefault() {
+        val subject = MainViewModel(
+                TitleRepository(
+                        MainNetworkFake(makeSuccessCall("OK")),
+                        TitleDaoFake("title")
+                )
+        )
+
+        assertThat(subject.title.getValueForTest()).isEqualTo("title")
+    }
+
+    @Test
+    fun whenSuccessfulTitleLoad_itShowsAndHidesSpinner() {
+        val call = FakeNetworkCall<String>()
+
+        val subject = MainViewModel(
+                TitleRepository(
+                        MainNetworkFake(call),
+                        TitleDaoFake("title")
+                )
+        )
+
+        subject.spinner.captureValues {
+            subject.onMainViewClicked()
+            runBlocking {
+                assertSendsValues(2_000, true)
+                call.onSuccess("data")
+                assertSendsValues(2_000, true, false)
+            }
+        }
+    }
+
+    @Test
+    fun whenErrorTitleReload_itShowsErrorAndHidesSpinner() {
+        val call = FakeNetworkCall<String>()
+        val subject = MainViewModel(
+                TitleRepository(
+                        MainNetworkFake(call),
+                        TitleDaoFake("title")
+                )
+        )
+
+        subject.spinner.captureValues {
+            val spinnerCaptor = this
+            subject.onMainViewClicked()
+            runBlocking {
+                assertSendsValues(2_000, true)
+                call.onError(FakeNetworkException("An error"))
+                assertSendsValues(2_000, true, false)
+            }
+        }
+    }
+
+    @Test
+    fun whenErrorTitleReload_itShowsErrorText() {
+        val call = FakeNetworkCall<String>()
+        val subject = MainViewModel(
+                TitleRepository(
+                        MainNetworkFake(call),
+                        TitleDaoFake("title")
+                )
+        )
+
+        subject.snackbar.captureValues {
+            subject.onMainViewClicked()
+            runBlocking {
+                call.onError(FakeNetworkException("An error"))
+                assertSendsValues(2_000, "An error")
+                subject.onSnackbarShown()
+                assertSendsValues(2_000, "An error", null)
+            }
+        }
+    }
+
+    @Test
+    fun whenMainViewClicked_titleIsRefreshed() {
+        val titleDao = TitleDaoFake("title")
+        val subject = MainViewModel(
+                TitleRepository(
+                        MainNetworkFake(makeSuccessCall("OK")),
+                        titleDao
+                )
+        )
+        subject.onMainViewClicked()
+        titleDao.assertNextInsert("OK")
+    }
+}
---
+++ kotlin-coroutines/coroutines/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.kotlincoroutines.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.kotlincoroutines.util.BACKGROUND
+
+/**
+ * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
+ * allows data to survive configuration changes such as screen rotations. In addition, background
+ * work such as fetching network results can continue through configuration changes and deliver
+ * results after the new Fragment or Activity is available.
+ */
+class MainViewModel : ViewModel() {
+
+    /**
+     * Request a snackbar to display a string.
+     *
+     * This variable is private because we don't want to expose MutableLiveData
+     *
+     * MutableLiveData allows anyone to set a value, and MainViewModel is the only
+     * class that should be setting values.
+     */
+    private val _snackBar = MutableLiveData<String>()
+
+    /**
+     * Request a snackbar to display a string.
+     */
+    val snackbar: LiveData<String>
+        get() = _snackBar
+
+    /**
+     * Wait one second then display a snackbar.
+     */
+    fun onMainViewClicked() {
+        // TODO: Replace with coroutine implementation
+        BACKGROUND.submit {
+            Thread.sleep(1_000)
+            // use postValue since we're in a background thread
+            _snackBar.postValue("Hello, from threads!")
+        }
+    }
+
+    /**
+     * Called immediately after the UI shows the snackbar.
+     */
+    fun onSnackbarShown() {
+        _snackBar.value = null
+    }
+}
